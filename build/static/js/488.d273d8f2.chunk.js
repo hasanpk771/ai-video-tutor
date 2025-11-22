d8157a24e6d1f810d46037687ca99fddc3b6a797
Anamul Hasan <your-email@example.com>
2025-11-22 23:23:59 +0600

    Force add full frontend project

--- build/static/js/488.d273d8f2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkai_tutor_frontend=self.webpackChunkai_tutor_frontend||[]).push([[488],{488:(e,t,n)=>{new WeakMap;new Set}}]);
-//# sourceMappingURL=488.d273d8f2.chunk.js.map
--- src/micTest.js
+++
@@ -1,4 +0,0 @@
-navigator.mediaDevices.getUserMedia({ audio: true })
-  .then(() => console.log("🎤 Microphone access OK"))
-  .catch(err => console.error("❌ Mic error:", err));
-
